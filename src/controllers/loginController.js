0ce3570d6602ccd8f7199ceb390584ae230c7896
Gabriel Rejtman <rejtmangabriel@gmail.com>
2024-03-01 14:08:24 -0400

    Delete src directory

--- src/controllers/loginController.js
+++
@@ -1,9 +0,0 @@
-import * as loginModels from '../models/loginModels.js';
-
-export const getDataUser = (req, res) => {
-   const createdLogin = loginModels.findLogin(req.body);
-
-   return res.status(201).json(createdLogin);
-}
-
-
--- src/server.js
+++
@@ -1,6 +0,0 @@
-import { app } from './app.js';
-
-const port = 3300 // substituir por porta usada no servidor local
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`)
-});
