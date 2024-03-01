9d011161b9583c62efbbd45b9acb339585707b7d
JorgeHenriqueBS <106774238+JorgeHenriqueBS@users.noreply.github.com>
2024-03-01 10:29:37 -0400

    Páginas e Components do Front

---
+++ pages/supervisor/LoginSupervisor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LoginSupervisor() {
+  return (
+    <div>Tela de login do supervisor</div>
+  )
+}
+
+export default LoginSupervisor
---
+++ pages/supervisor/ProblemNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProblemNotFound() {
+  return (
+    <div>ProblemNotFound</div>
+  )
+}
+
+export default ProblemNotFound
