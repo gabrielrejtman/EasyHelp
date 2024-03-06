899edc453f8d67e8f4feedde896d96cbfe8396cf
William Matheus <108390581+willmatheus@users.noreply.github.com>
2024-03-06 04:50:08 -0400

    Delete Back directory

--- Back/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-const port = 3300 // substituir por porta usada no servidor local
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`)
-});
